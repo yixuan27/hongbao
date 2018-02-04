f27394e5face80a590810944e36afd48a909a414
zhuweiyou <i@zhuweiyou.com>
2018-02-04 14:58:46 +0800

    完成拼手气红包脚本

---
+++ README.md
@@ -0,0 +1,5 @@
+# 饿了么
+
+[![GPL-3.0](https://img.shields.io/badge/license-GPL--3.0-blue.svg)](LICENSE)
+
+- [拼手气红包](拼手气红包) 一键领取最大幸运红包
---
+++ 拼手气红包/src/request.js
@@ -0,0 +1,62 @@
+const axios = require('axios')
+const querystring = require('querystring')
+const cookie = require('./cookie')
+
+const request = axios.create({
+  baseURL: `https://h5.ele.me/`,
+  headers: {
+    origin: 'https://h5.ele.me',
+    referer: 'https://h5.ele.me/hongbao/',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; PRO 6 Build/MRA58K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/53.0.2785.49 Mobile MQQBrowser/6.2 TBS/043221 Safari/537.36 V1_AND_SQ_7.0.0_676_YYB_D QQ/7.0.0.3135 NetType/WIFI WebP/0.3.0 Pixel/1080'
+  }
+})
+
+module.exports = async ({
+  mobile,
+  url
+}) => {
+  let index = 0
+  const query = querystring.parse(url)
+  await lottery()
+
+  async function lottery(phone) {
+    phone = phone || `138${String(Math.random() * 10).slice(-8)}`
+    console.log(phone)
+    // 绑定手机号
+    await request.put(`/restapi/v1/weixin/${cookie[index].openid}/phone`, {
+      sign: cookie[index].eleme_key,
+      phone
+    })
+
+    // 领红包
+    const {
+      data: {
+        promotion_records = []
+      }
+    } = await request.post(`/restapi/marketing/promotion/weixin/${cookie[index].openid}`, {
+      device_id: '',
+      group_sn: query.sn,
+      hardware_id: '',
+      method: 'phone',
+      phone,
+      platform: query.platform,
+      sign: cookie[index].eleme_key,
+      track_id: '',
+      unionid: 'fuck',
+      weixin_avatar: '',
+      weixin_username: ''
+    })
+
+    // 计算剩余几个为幸运红包
+    const number = query.lucky_number - promotion_records.length
+    if (number <= 0) {
+      console.log('幸运红包已被领取')
+      return
+    }
+
+    console.log(`还要领 ${number} 个红包`)
+    index++
+    // 如果是幸运红包，指定手机号领取
+    await lottery(number === 1 ? mobile : null)
+  }
+}

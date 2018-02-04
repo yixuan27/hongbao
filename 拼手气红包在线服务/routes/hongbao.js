8b7a1c5f739801b5cc3fec30ea58bd092a39e2b9
zhuweiyou <i@zhuweiyou.com>
2018-02-04 15:58:11 +0800

    增加在线服务

---
+++ 拼手气红包在线服务/routes/hongbao.js
@@ -0,0 +1,36 @@
+var express = require('express')
+var router = express.Router()
+var request = require('../../拼手气红包/src/request')
+
+/* GET users listing. */
+router.post('/', async (req, res, next) => {
+  const {
+    url,
+    mobile
+  } = req.body
+  if (!url || !mobile) {
+    res.json({
+      message: '请将信息填写完整'
+    })
+  }
+  if (!/^1\d{10}$/.test(mobile)) {
+    res.json({
+      message: '请填写 11 位手机号码'
+    })
+  }
+  try {
+    await request({
+      url,
+      mobile
+    })
+    res.json({
+      message: '领取成功'
+    })
+  } catch (e) {
+    res.json({
+      message: e.message
+    })
+  }
+})
+
+module.exports = router

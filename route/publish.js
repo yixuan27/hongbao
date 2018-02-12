452df0d60d8d8971f8c9c77a0e05f00bfe1f7fc4
zhuweiyou <i@zhuweiyou.com>
2018-02-12 11:39:05 +0800

    更新部署脚本

--- route/publish.js
+++ route/publish.js
@@ -5,7 +5,7 @@ const path = require('path')
 
 router.post('/', async (req, res, next) => {
   if (req.body.key === process.env.ELEME_PUBLISH_KEY) {
-    exec(path.resolve('..', 'publish.sh'))
+    exec(path.resolve(__dirname, '..', 'publish.sh'))
     res.json({message: '正在发布中，稍后查看效果'})
   } else {
     res.json({message: '密钥不正确，不执行发布'})

961a46c8eb310d97f6a4f30441d3a584bcc9d74b
zhuweiyou <i@zhuweiyou.com>
2018-02-26 14:51:50 +0800

    为了支持短链接，增加渠道选择

--- route/publish.js
+++ route/publish.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const path = require('path')
 
 router.post('/', async (req, res, next) => {
-  if (req.body.key === process.env.ELEME_PUBLISH_KEY) {
+  if (req.body.indexOf(process.env.ELEME_PUBLISH_KEY) !== -1) {
     exec(path.resolve(__dirname, '..', 'publish.sh'))
     res.json({message: '正在发布中，稍后查看效果'})
   } else {

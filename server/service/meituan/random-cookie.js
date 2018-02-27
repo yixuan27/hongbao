0316a74d26328592d9dbe3b1bd68d6eed5ba42db
zhuweiyou <i@zhuweiyou.com>
2018-02-27 10:53:39 +0800

    昵称手机号打码

--- server/service/meituan/random-cookie.js
+++ server/service/meituan/random-cookie.js
@@ -2,7 +2,13 @@ const randomNickname = require('./random-nickname')
 const randomOpenid = require('./random-openid')
 const crypto = require('./crypto')
 
-module.exports = (nickname = randomNickname()) => {
+module.exports = (nickname = '') => {
+  if (nickname.length === 11) {
+    // 当使用大号手机号领取时，昵称用手机号（这样在领取成功时能看到弹出），中间 4 位打码
+    nickname = nickname.replace(/(\d{3})(\d{4})(\d{3})/, '$1****$3')
+  } else {
+    nickname = randomNickname()
+  }
   const ewxinfo = {
     nickname,
     imgUrl: '',

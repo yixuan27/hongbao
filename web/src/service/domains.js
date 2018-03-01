5cdb50578d3adb49ac1e406519531847810e4e09
zhuweiyou <i@zhuweiyou.com>
2018-03-01 13:38:57 +0800

    增加服务器

--- web/src/service/domains.js
+++ web/src/service/domains.js
@@ -1,5 +1,6 @@
 export default [
   'https://hongbao.xxooweb.com',
   'http://hongbao.dingjian.name',
-  'http://101.226.171.170:3007'
+  'http://101.226.171.170:3007',
+  'http://45.77.29.9:3007'
 ]

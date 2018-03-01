fbf420f2bd686f826d3fa1a3387b6747dd3ad7f7
Lentin <codexss@users.noreply.github.com>
2018-03-01 14:44:07 +0800

    Update domains.js (#29)

--- web/src/service/domains.js
+++ web/src/service/domains.js
@@ -2,5 +2,6 @@ export default [
   'https://hongbao.xxooweb.com',
   'http://hongbao.dingjian.name',
   'http://101.226.171.170:3007',
-  'http://45.77.29.9:3007'
+  'http://45.77.29.9:3007',
+  'http://hongbao.lte.pw:3007',
 ]

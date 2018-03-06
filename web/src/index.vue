865bc3e968b0da1631cde65d78920c45c0c327fc
zhuweiyou <i@zhuweiyou.com>
2018-03-06 20:16:55 +0800

    增加支付宝红包码

--- web/src/index.vue
+++ web/src/index.vue
@@ -21,15 +21,16 @@
       <div class="form-group">
         <label>选择服务器线路</label>
         <select class="form-control" v-model="domain">
-          <option v-for="(url, index) in domains" :value="index">
+          <option v-for="(url, index) in domains" :key="index" :value="index">
             {{index + 1}}号服务器{{index === 0 ? '（官服）' : ''}}
           </option>
         </select>
       </div>
       <div class="form-group">
         <button type="button" class="btn btn-danger submit" :disabled="submit" @click="getHongbao">
-          {{submit ? '正在领取...' : '马上领取'}}
+          {{submit ? '正在领取...' : '领取手气最佳红包'}}
         </button>
+        <button type="button" class="btn btn-default ali-hongbao-m">支付宝天天领红包</button>
       </div>
       <ul class="breadcrumb">
         <li>
@@ -53,12 +54,17 @@
         <p class="text-center"><b>红包分享交流微信群</b><br>请加上面的微信号邀请你入群<br>加群的朋友非常多，请耐心等待通过</p>
       </div>
     </div>
+    <div class="ali-hongbao-pc">
+      <img src="./static/alihongbao.jpg">
+      <p>支付宝天天领红包</p>
+    </div>
   </div>
 </template>
 
 <script>
   import 'bootstrap/dist/css/bootstrap.css'
   import axios from 'axios'
+  import ClipboardJS from 'clipboard'
   import domains from './service/domains'
 
   export default {
@@ -70,6 +76,9 @@
         domains,
         domain: 0
       }
+    },
+    mounted () {
+      this.aliHongbao()
     },
     methods: {
       async getHongbao () {
@@ -87,6 +96,20 @@
         }
         this.submit = false
         localStorage.setItem('mobile', mobile)
+      },
+      aliHongbao () {
+        const clipboard = new ClipboardJS('.ali-hongbao-m', {
+          text: () => 'RY2WVP09sm'
+        })
+        clipboard.on('success', e => {
+          alert('打开支付宝即可领取红包（每天仅一次）')
+          e.clearSelection()
+        })
+        clipboard.on('error', e => {
+          if (confirm('您的设备不支持复制红包码，是否跳转到支付宝领取？')) {
+            window.location.href = 'https://render.alipay.com/p/f/fd-j6lzqrgm/guiderofmklvtvw.html?shareId=2088012515595090&campStr=p1j%2BdzkZl018zOczaHT4Z5CLdPVCgrEXq89JsWOx1gdt05SIDMPg3PTxZbdPw9dL&sign=wFumSPvgk8JDVbYta9%2FILuhrUmaW%2FpXfsnKt%2FUzDj8o%3D&scene=offlinePaymentNewSns'
+          }
+        })
       }
     }
   }
@@ -101,15 +124,6 @@
   .breadcrumb {
     margin-bottom: 10px;
     white-space: nowrap;
-  }
-
-  @media screen and (max-width: 480px) {
-    .panel {
-      width: 100%;
-      margin: 0;
-      border: 0;
-      box-shadow: none;
-    }
   }
 
   .submit,
@@ -138,7 +152,48 @@
     line-height: 1.5;
 
     span {
-      color: #aaa;
+      color: #666;
+    }
+  }
+
+  .ali-hongbao-pc {
+    position: fixed;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 50%;
+    margin-left: -500px;
+    width: 250px;
+    z-index: 10;
+    text-align: center;
+    border: 1px solid #ccc;
+    padding: 10px;
+    border-radius: 4px;
+
+    p {
+      padding-top: 20px;
+    }
+  }
+
+  .ali-hongbao-m {
+    display: none;
+  }
+  
+  @media screen and (max-width: 480px) {
+    .panel {
+      width: 100%;
+      margin: 0;
+      border: 0;
+      box-shadow: none;
+    }
+
+    .ali-hongbao-pc {
+      display: none;
+    }
+
+    .ali-hongbao-m {
+      display: block;
+      width: 100%;
+      margin-top: 15px;
     }
   }
 </style>
